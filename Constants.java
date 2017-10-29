0405f851d42fa4064786871878ecddf504273cb1
cubelith <cubelith@gmail.com>
2017-10-29 20:45:59 +0100

    Repulse Prototype One

--- Constants.java
+++ Constants.java
@@ -1,6 +1,5 @@
 package com.example.kuba.repulsev001;
 
-
 /**
  * The class for keeping various constants, like the screen size.
  * That does not include constants we set ourselves, only the independent ones.
